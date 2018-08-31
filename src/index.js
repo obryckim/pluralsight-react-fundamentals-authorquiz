e6c866997699b0f2cd32ee7a275834db2ba4427c
Michael Obrycki <obryckim@users.noreply.github.com>
2018-08-31 05:54:07 -0400

    setup author quiz basic logic

--- src/index.js
+++ src/index.js
@@ -3,6 +3,66 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import registerServiceWorker from './registerServiceWorker';
+import { shuffle, sample } from 'underscore';
 
-ReactDOM.render(<AuthorQuiz />, document.getElementById('root'));
+const authors = [
+    {
+        name: 'Mark Twain',
+        imageUrl: 'images/authors/mark-twain.jpg',
+        imageSrc: 'http://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Mark_Twain%2C_Brady-Handy_photo_portrait%2C_Feb_7%2C_1871%2C_cropped.jpg/400px-Mark_Twain%2C_Brady-Handy_photo_portrait%2C_Feb_7%2C_1871%2C_cropped.jpg',
+        books: ['The Adventures of Huckleberry Finn']
+    },
+    {
+        name: 'Joseph Conrad',
+        imageUrl: 'images/authors/joseph-conrad.png',
+        imageSrc: 'http://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Joseph_Conrad.PNG/350px-Joseph_Conrad.PNG',
+        books: ['Heart of Darkness']
+    },
+    {
+        name: 'J.K. Rowling',
+        imageUrl: 'images/authors/jk-rowling.jpg',
+        imageSrc: 'http://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/J._K._Rowling_2010.jpg/440px-J._K._Rowling_2010.jpg',
+        books: ['Harry Potter and the Sorcerers Stone']
+    },
+    {
+        name: 'Stephen King',
+        imageUrl: 'images/authors/stephen-king.jpg',
+        imageSrc: 'http://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Stephen_King%2C_Comicon.jpg/440px-Stephen_King%2C_Comicon.jpg',
+        books: ['The Shining', 'IT']
+    },
+    {
+        name: 'Charles Dickens',
+        imageUrl: 'images/authors/charles-dickens.jpg',
+        imageSrc: 'http://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Dickens_Gurney_head.jpg/440px-Dickens_Gurney_head.jpg',
+        books: ['David Copperfield', 'A Tale of Two Cities']
+    },
+    {
+        name: 'William Shakespeare',
+        imageUrl: 'images/authors/william-shakespeare.jpg',
+        imageSrc: 'http://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Shakespeare.jpg/500px-Shakespeare.jpg',
+        books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
+    }
+];
+
+function getTurnData(authors) {
+    const allBooks = authors.reduce(function (p, c, i) {
+        return p.concat(c.books);
+    }, []);
+
+    const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+    const answer = sample(fourRandomBooks);
+
+    return {
+        books: fourRandomBooks,
+        author: authors.find((author) =>
+            author.books.some((title) =>
+                title === answer))
+    };
+}
+
+const state = {
+    turnData: getTurnData(authors)
+};
+
+ReactDOM.render(<AuthorQuiz {...state} />, document.getElementById('root'));
 registerServiceWorker();
